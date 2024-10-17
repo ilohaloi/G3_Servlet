9b118381b0d7e0ec047dc6d5749104e023893d93
kent <yesa1001117@gmail.com>
2024-10-17 19:23:08 +0800

    10/17上傳資料

--- src/main/java/com/laiutil/vault/VaultFuntion.java
+++ src/main/java/com/laiutil/vault/VaultFuntion.java
@@ -10,55 +10,55 @@ import com.bettercloud.vault.response.LogicalResponse;
 import kotlin.Pair;
 
 public class VaultFuntion {
-	private Vault vault;
-	private String path;
-
-	public VaultFuntion(Vault vault, String path) {
-		this.vault = vault;
-		this.path = path;
-	}
-
-	public Map<String, String> getAllData() {
-		LogicalResponse readResponse = null;
-		try {
-			readResponse = vault.logical().read(path);
-		} catch (VaultException e) {
-			e.printStackTrace();
-		}
-		return readResponse.getData();
-	}
-	public Pair<String, String> getUserData(String name) {
-		var data = getAllData();
-		Pair<String, String> userPair = null;
-		for (Map.Entry<String, String> entry : data.entrySet()) {
-			if(entry.getKey().equals(name))
-			{
-//				userPair.copy(entry.getKey(), entry.getValue());
-				userPair = new Pair<String, String>(entry.getKey(),entry.getValue());
-				break;
-			}
-		}
-		return userPair;
-	}
-	
-	/**
-	 * 更新新增共用同一個API
-	 *
-	 * @throws VaultException
-	 *
-	 */
-	
-	public void update(Pair<String, String> data) throws VaultException {
-		Map<String, String> existingData = getAllData();
-		existingData.put(data.getFirst(), data.getSecond());
-		vault.logical().write(path, new HashMap<String, Object>(existingData));
-	}
-
-	public String getPath() {
-		return path;
-	}
-
-	public void setPath(String path) {
-		this.path = path;
-	}
+//	private Vault vault;
+//	private String path;
+//
+//	public VaultFuntion(Vault vault, String path) {
+//		this.vault = vault;
+//		this.path = path;
+//	}
+//
+//	public Map<String, String> getAllData() {
+//		LogicalResponse readResponse = null;
+//		try {
+//			readResponse = vault.logical().read(path);
+//		} catch (VaultException e) {
+//			e.printStackTrace();
+//		}
+//		return readResponse.getData();
+//	}
+//	public Pair<String, String> getUserData(String name) {
+//		var data = getAllData();
+//		Pair<String, String> userPair = null;
+//		for (Map.Entry<String, String> entry : data.entrySet()) {
+//			if(entry.getKey().equals(name))
+//			{
+////				userPair.copy(entry.getKey(), entry.getValue());
+//				userPair = new Pair<String, String>(entry.getKey(),entry.getValue());
+//				break;
+//			}
+//		}
+//		return userPair;
+//	}
+//	
+//	/**
+//	 * 更新新增共用同一個API
+//	 *
+//	 * @throws VaultException
+//	 *
+//	 */
+//	
+//	public void update(Pair<String, String> data) throws VaultException {
+//		Map<String, String> existingData = getAllData();
+//		existingData.put(data.getFirst(), data.getSecond());
+//		vault.logical().write(path, new HashMap<String, Object>(existingData));
+//	}
+//
+//	public String getPath() {
+//		return path;
+//	}
+//
+//	public void setPath(String path) {
+//		this.path = path;
+//	}
 }
